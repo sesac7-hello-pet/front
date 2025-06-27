15a969f5b974af6c4c608c98e23ba320f6149514
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-28 01:21:37 +0900

    feat: Navigator작성[#-front-4]

---
+++ app/components/Navigator.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Navigator() {
+  return (
+    <nav className="flex justify-between items-center border-b border-black-500 p-4">
+      <Link href="/" className="text-lg font-semibold">
+        Home
+      </Link>
+
+      <div>
+        <Link
+          href="/login"
+          className="px-4 py-2 border border-gray-300 rounded text-sm"
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
+}
