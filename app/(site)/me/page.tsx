290e22be68e87acc90e38ee2d213c88703639173
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-06-30 10:40:55 +0900

    feat: 유저 페이지 현재 유저 정보 보기[#front-9]

--- app/(site)/me/page.tsx
+++ app/(site)/me/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { useUserStore } from "@/app/store/UserStore";
+
 export default function MyPage() {
-  return <div>hi</div>;
+  const user = useUserStore((s) => s.user);
+
+  return (
+    <div className="flex flex-col items-center justify-start h-screen bg-gray-50 pt-[5vh]">
+      <img
+        src={user?.profileUrl}
+        alt="Profile"
+        className="w-32 h-32 rounded-full object-cover"
+      />
+
+      <div className="mt-6 text-center">
+        <p className="text-xl font-semibold text-gray-800">{user?.nickname}</p>
+        <p className="mt-1 text-sm text-gray-500">{user?.email}</p>
+      </div>
+    </div>
+  );
 }
