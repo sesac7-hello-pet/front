915a5089f7eb04d213b8321d7a841d8fd93f66cb
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-01 18:54:31 +0900

    feat: 마운트시 유저 정보 가져오기[#front-11]

--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -1,3 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import api from "../lib/api";
+
+interface User {
+  id: number;
+  email: string;
+  role: string;
+  nickname: string;
+  username: string;
+  phoneNumber: string;
+}
+
 export default function UserList() {
-  return <div>1</div>;
+  const [users, setUsers] = useState<User[]>();
+
+  useEffect(() => {
+    getUserList();
+  }, []);
+
+  async function getUserList() {
+    const res = api.get("/users");
+    setUsers((await res).data);
+  }
+
+  return (
+    <ul>
+      {users?.map((user) => (
+        <li key={user.id}>
+          {user.role}
+          {user.username}
+          {user.email}
+          {user.nickname}
+          {user.phoneNumber}
+        </li>
+      ))}
+    </ul>
+  );
 }
