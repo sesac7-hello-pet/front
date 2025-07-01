bee0c326896bd9aa6e6c17ba71b58f9a9cb27940
ryukyungwoo <ryukyungwoo1220@gmail.com>
2025-07-01 11:29:47 +0900

    feat: announcement 페이지 스케치[#front-12]

--- app/(site)/announcements/page.tsx
+++ app/(site)/announcements/page.tsx
@@ -1,3 +1,49 @@
-export default function page() {
-  return <div>announcement page</div>;
+"use client";
+
+import api from "@/app/lib/api";
+import { useEffect, useState } from "react";
+
+interface Announcement {
+  id: number;
+  status: string;
+  image: string;
+  breed: string;
+}
+
+export default function AnnouncementsPage() {
+  const [announcements, setAnnouncements] = useState<Announcement[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getAnnouncement();
+  }, []);
+
+  async function getAnnouncement() {
+    try {
+      const res = await api.get("/announcements");
+      setAnnouncements(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false); // 무조건 로딩 종료
+    }
+  }
+
+  if (loading) return <h1>Loading…</h1>;
+
+  return (
+    <ul className="grid grid-cols-4 gap-4">
+      {announcements.map((announcement) => (
+        <li
+          key={announcement.id}
+          className="flex flex-col items-center space-y-1 rounded-lg border p-2 shadow-sm"
+        >
+          <img src={announcement.image} />
+          <p className="text-sm font-medium">breed: {announcement.breed}</p>
+          <p className="text-xs text-gray-600">status: {announcement.status}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
