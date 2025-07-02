d829ea39273f510c7b2f655f60445d37e531d0de
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-07-02 15:44:01 +0900

    feat: 카테고리 및 동물 종류 선택 버튼 생성 [#front-14]

--- app/(site)/boards/new/page.tsx
+++ app/(site)/boards/new/page.tsx
@@ -1,3 +1,19 @@
 export default function BoardCreate() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <p>
+          카테고리
+          <button>커뮤니티</button>
+          <button>Q&A</button>
+        </p>
+        <p>
+          동물 종류
+          <button>강아지</button>
+          <button>고양이</button>
+          <button>기타</button>
+        </p>
+      </div>
+    </div>
+  );
 }
