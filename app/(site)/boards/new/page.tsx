4f7b8ab038790d8a5e60fc2820440de8ed596b77
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-07-02 19:42:07 +0900

    feat:버튼별 클릭 시  CSS 변경 [#front-14]

--- app/(site)/boards/new/page.tsx
+++ app/(site)/boards/new/page.tsx
@@ -1,19 +1,81 @@
+"use client";
+
+import api from "@/app/lib/api";
+import React, { useState } from "react";
+
 export default function BoardCreate() {
+  const [category, setCategory] = useState<string>("커뮤니티");
+  const [petType, setPetType] = useState<string>("강아지");
+
+  const changeButton = (state: string, current: string) =>
+    `w-40 rounded-lg font-semibold py-2 shadow-sm transition ${
+      state == current
+        ? "bg-amber-400 text-white"
+        : "bg-amber-50 text-amber-300 hover:bg-amber-100"
+    }`;
+
+  function clickCategory(selectd: string) {
+    setCategory(selectd);
+  }
+
+  function clickPetType(selectd: string) {
+    setPetType(selectd);
+  }
+
+  async function createBoard(e: React.FormEvent) {
+    e.preventDefault();
+    const payload = {
+      title: "title",
+      content: "content",
+      img_url: "img_url",
+      boardCategory: "QNA",
+      petType: "DOG",
+    };
+
+    const res = await api.post("/boards", payload);
+    console.log(res.data);
+  }
+
   return (
     <div>
       <div>
         <p>
           카테고리
-          <button>커뮤니티</button>
-          <button>Q & A</button>
+          <button
+            onClick={() => clickCategory("커뮤니티")}
+            className={changeButton("커뮤니티", category)}
+          >
+            커뮤니티
+          </button>
+          <button
+            onClick={() => clickCategory("Q & A")}
+            className={changeButton("Q & A", category)}
+          >
+            Q & A
+          </button>
         </p>
         <p>
           동물 종류
-          <button>강아지</button>
-          <button>고양이</button>
-          <button>기타</button>
+          <button
+            onClick={() => clickPetType("강아지")}
+            className={changeButton("강아지", petType)}
+          >
+            강아지
+          </button>
+          <button
+            onClick={() => clickPetType("고양이")}
+            className={changeButton("고양이", petType)}
+          >
+            고양이
+          </button>
+          <button
+            onClick={() => clickPetType("기타")}
+            className={changeButton("기타", petType)}
+          >
+            기타
+          </button>
         </p>
-        <form>
+        <form onSubmit={createBoard}>
           <p>
             <input type="text" placeholder="제목을 입력해주세요." />
           </p>
@@ -25,7 +87,7 @@ export default function BoardCreate() {
             <input type="file" />
             <button>사진첨부</button>
           </p>
-          <button>등록하기</button>
+          <button type="submit">등록하기</button>
         </form>
       </div>
     </div>
