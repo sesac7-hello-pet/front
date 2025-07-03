b8b771a07a61a8dc5f398e2bac171e7c4acdd22c
DESKTOP-GR883KV\user <mirae3951@gmail.com>
2025-07-03 17:14:47 +0900

    feat:게시글 리스트 마운트 [#front-18]

--- app/(site)/boards/page.tsx
+++ app/(site)/boards/page.tsx
@@ -3,11 +3,39 @@
 import api from "@/app/lib/api";
 import { useEffect, useState } from "react";
 
+enum Category {
+  TOTAL = "전체",
+  FREE = "커뮤니티",
+  QNA = "Q & A",
+}
+
+enum petType {
+  DOG = "강아지",
+  CAT = "고양이",
+  ETC = "기타",
+}
+
+interface BoardPage {
+  page: number;
+  size: number;
+  totalPages: number;
+  totalCount: number;
+  boardList: Board[];
+}
+
 interface Board {
   id: number;
   nickname: string;
   title: string;
+  content: string;
+  image_url: string;
+  likesCount: number;
+  viewsCount: number;
+  commentsCount: number;
   createdAt: string;
+  updatedAt: string;
+  category: string;
+  petType: string;
 }
 
 export default function BoardListPage() {
@@ -36,7 +64,13 @@ export default function BoardListPage() {
     <ul>
       {boards.map((board) => (
         <li key={board.id}>
-          {board.nickname}, {board.title} | {board.createdAt}
+          {Category[board.category as keyof typeof Category]}
+          {petType[board.petType as keyof typeof petType]}
+          {board.title}
+          {board.content}
+          {board.nickname}
+          {board.viewsCount}|{board.likesCount}|{board.commentsCount}
+          {board.createdAt.split("T")[0]}{" "}
         </li>
       ))}
     </ul>
